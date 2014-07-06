a938101a5060af6fcabcd026deb14eb41c28303d
Michell Stuttgart <michellstut@gmail.com>
2014-07-06 03:58:43 +0000

    1. Apenas os diretorios com o codigo fonte, o UML e o diretorio da monografia, apresentação e painel forma mantidos. O restante foi deletado.

---
+++ Saga_Game_Library_Doc/Monografia_TCC_UTF8_Revisada/Monografia Latex/CodigoFonte/ExemploAnimatedSprite.cpp
@@ -0,0 +1,20 @@
+// Declaramos o sprite
+AnimatedSprite spr;
+// Carregamos o arquivo tmx
+spr.load( "personagem.tmx" );
+// Ajustamos posicao do sprite
+spr.setPosition( Vector2D( 100.0f, 100.0f ) );
+// Colocamos o sprite como visible
+spr.setVisible( true );
+
+while( true )
+{
+    // Desenhamos o sprite
+    spr.draw();
+    // Avancamos para o proximo frame
+    spr.nextFrame();
+    // Atualizamos o display
+    video.refresh();
+    // Aguarda alguns milesegundos
+    al_rest( 0.06 );
+}
