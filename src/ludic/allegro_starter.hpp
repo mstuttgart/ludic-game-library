b9fa1b33e77bd6102c6a5e94c550e0b483b9e3c2
michell <michellstut@gmail.com>
2014-07-27 17:00:33 -0300

    Doxygen comment updated

--- src/ludic/allegro_starter.hpp
+++ src/ludic/allegro_starter.hpp
@@ -21,8 +21,9 @@ namespace Ludic {
  * @author Michell Stuttgart
  * @date 01/09/14
  * @class AllegroStarter
- * @brief Class responsible for initializing the basic modules 
- * of the Allegro, Allegro_Image, Allegro_Font, Allegro_Audio 
+ * @brief Class responsible for initializing the basic modules of the Allegro.
+ * 
+ * The basic modules are: Allegro_Image, Allegro_Font, Allegro_Audio 
  * and Allegro_Primitives.  
  * It is called automatically by Ludic Library 
  * in early in the program. This is a class for the exclusive 
