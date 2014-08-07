dd5a99e6025420ae00fd113091069da4d0fae5bc
MStuttgart <michellstut@gmail.com>
2014-08-07 11:35:27 -0300

    Color class doc is done.

--- src/ludic/main_default.cpp
+++ src/ludic/main_default.cpp
@@ -16,9 +16,10 @@ using namespace Ludic;
 using namespace std;
 
 /* Constantes do sprite*/
-#define FPS 90
+#define FPS 60
 
-int main() {
+int main()
+{
 
 	//---------------------------
 
@@ -26,7 +27,7 @@ int main() {
 
 	video.setTitle( "Saga Game Library" );
 	video.setIcon( "Resource/icone.png" );
-	
+
 	video.setBackgroundColor( Color(145,184,60) );
 
 	//-----------------------------------------------
@@ -44,18 +45,18 @@ int main() {
 
 	// Colocamos o sprite como visible
 	spr.setVisible( true );
-	
+
 	//------------------------------------------
-	
+
 	StaticSprite spr1;
 	StaticSprite spr2;
-	
+
 	spr1.load( "Resource/icone.png" );
 	spr2.load( "Resource/circle.png" );
-	
+
 	spr1.setVisible( true );
 	spr2.setVisible( true );
-	
+
 	spr1.setPosition( Vector2D( 200.0f, 200.0f ) );
 	spr2.setPosition( Vector2D( 200.0f, 300.0f ) );
 
@@ -81,7 +82,7 @@ int main() {
 
 	if( mapa.load( "Resource/rpg/mapa.tmx" ) )
 		cout << "Carregou mapa com sucesso" << endl;
-		
+
 	mapa.setScreenDimension( video.getWidth(), video.getHeight() );
 
 	//-------------------------------------------
@@ -97,7 +98,7 @@ int main() {
 	layerManager.push_back( mapa.getLayer( "Colisao" ) );
 
 	TiledLayer* l = mapa.getLayer( "Colisao" );
-	
+
 	//-----------------------------------------
 
 	int movex = 0;
@@ -107,157 +108,125 @@ int main() {
 
 	double div = 1.0 / FPS;
 	bool sair  = false;
+	
+	//-----------------------------------------
 
 	TimeHandler fpsTimer;
 
-	//-----------------------------------------
-
-	ALLEGRO_EVENT_QUEUE *event_queue = al_create_event_queue();
-	ALLEGRO_TIMER *timer             = al_create_timer( div );
-
-	al_register_event_source( event_queue, al_get_display_event_source( video ) );
-	al_register_event_source( event_queue, al_get_timer_event_source( timer ) );
-
-	bool redraw = false;
-
 	// Capturamos o tempo inicial
-	//fpsTimer.start();
+	fpsTimer.start();
 
-	//double cTime;
+	double cTime;
 
 	Vector2D desloc( 0 , 0 );
 	
-	al_start_timer(timer);
+	musica.play();
+	musica.setLoopingMode( AudioPlayMode::PLAY_LOOP );
+	
+	int deslocamento = 5;
 
 	while( !sair ) {
-
-		ALLEGRO_EVENT ev;
 		
-		al_wait_for_event( event_queue, &ev );
+		cTime = fpsTimer.getTicks();
 
-		if( ev.type == ALLEGRO_EVENT_DISPLAY_CLOSE ) {
+		// Realiza um tipo de snapshoot no estado das teclas
+		keyboard->update();
+
+		// Atualizamoa posicao do personagem de acordo com o sprite
+		movex = movey = 0;
+
+		if( keyboard->keyPressed( KeyCode::KEY_RIGHT ) ) {
+			spr.setCurrentAnimation( "Direita" );
+			movex = deslocamento;
+		}
+
+		if( keyboard->keyPressed( KeyCode::KEY_LEFT ) ) {
+			spr.setCurrentAnimation( "Esquerda" );
+			movex = -deslocamento;
+		}
+
+		if( keyboard->keyPressed( KeyCode::KEY_UP ) ) {
+			spr.setCurrentAnimation( "Costas" );
+			movey = -deslocamento;
+		}
+
+		if( keyboard->keyPressed( KeyCode::KEY_DOWN ) ) {
+			spr.setCurrentAnimation( "Frente" );
+			movey = deslocamento;
+		}
+
+		if( keyboard->keyRelease( KeyCode::KEY_ESCAPE ) ) {
 			sair = true;
 		}
-		else if( ev.type == ALLEGRO_EVENT_TIMER ) {
 
-			// Realiza um tipo de snapshoot no estado das teclas
-			keyboard->update();
+		if( keyboard->keyTyped( KeyCode::KEY_SPACE ) ) {
+			texto.setText( "play" );
+		} else if( keyboard->keyTyped( KeyCode::KEY_P ) ) {
+			musica.pause();
+			texto.setText( "pause" );
+		} else if( keyboard->keyTyped( KeyCode::KEY_S ) ) {
+			musica.stop();
+			texto.setText( "stop" );
+		}
 
-			// Atualizamoa posicao do personagem de acordo com o sprite
-			movex = movey = 0;
+		if( keyboard->keyTyped( KeyCode::KEY_M ) ) {
+			sample.play();
+			sample2.play();
+		}
 
-			if( keyboard->keyPressed( KeyCode::KEY_RIGHT ) ) {
-				spr.setCurrentAnimation( "Direita" );
-				movex = 1;
-			}
-			
-			if( keyboard->keyPressed( KeyCode::KEY_LEFT ) ) {
-				spr.setCurrentAnimation( "Esquerda" );
-				movex = -1;
-			}
-			
-			if( keyboard->keyPressed( KeyCode::KEY_UP ) ) {
-				spr.setCurrentAnimation( "Costas" );
-				movey = -1;
-			}
-			
-			if( keyboard->keyPressed( KeyCode::KEY_DOWN ) ) {
-				spr.setCurrentAnimation( "Frente" );
-				movey = 1;
-			}
-			
-			if( keyboard->keyRelease( KeyCode::KEY_ESCAPE ) ) {
-				sair = true;
-			}
+		if ( keyboard->keyTyped( KeyCode::KEY_V ) ) {
+			l->setVisible( !l->isVisible() );
+		}
 
-			if( keyboard->keyTyped( KeyCode::KEY_SPACE ) ) {
-				musica.play();
-				texto.setText( "play" );
-			}
-			else if( keyboard->keyTyped( KeyCode::KEY_P ) ) {
-				musica.pause();
-				texto.setText( "pause" );
-			}
-			else if( keyboard->keyTyped( KeyCode::KEY_S ) ) {
-				musica.stop();
-				texto.setText( "stop" );
-			}
+		// Verificamos se podemos nos movimentar no eixo X
+		if( l->checkCollision( spr, movex, 0, 2 ) ) {
+			movex = 0;
+		}
 
-			if( keyboard->keyTyped( KeyCode::KEY_M ) ) {
-				sample.play();
-				sample2.play();
-			}
+		if( l->checkCollision( spr, 0, movey, 2 ) ) {
+			movey = 0;
+		}
 
-			if ( keyboard->keyTyped( KeyCode::KEY_V ) ) {
-				l->setVisible( !l->isVisible() );
-			}
+		//------------------------------------------
 
-			// Verificamos se podemos nos movimentar no eixo X
-			if( l->checkCollision( spr, movex, 0, 2 ) ) {
-				movex = 0;
-			}
+		if( spr.collidesWithPixel( &spr2 ) ) {
+			texto.setText( "Colisao" );
+		} else {
+			texto.setText("");
+		}
 
-			if( l->checkCollision( spr, 0, movey, 2 ) ) {
-				movey = 0;
-			}
-			
-			//------------------------------------------
-			
-			if( spr.collidesWithPixel( &spr2 ) )
-			{
-				texto.setText( "Colisao" );
-			}
-			else{
-				texto.setText("");
-			}
-			
-			//------------------------------------------
-			
-			if( movex || movey ) {
-				desloc.setCoordinates( movex, movey );
-				spr.move( desloc );
-				spr.nextFrame();
-				//spr1.move( desloc );
-				
-			}
+		//------------------------------------------
 
-			redraw = true;
-			
-		}//if timer
-
-		if( redraw && al_is_event_queue_empty( event_queue ) ) {
-
-			redraw = false;
-
-			// Desenhamos cada uma das camadas
-			al_hold_bitmap_drawing( true );
-
-			for( unsigned int i = 0; i < layerManager.size(); i++ ) {
-				layerManager.at( i )->draw();
-			}
-
-			texto.drawText();
-
-			al_hold_bitmap_drawing( false );
-
-			// Atualizamos a tela
-			video.refresh();
+		if( movex || movey ) {
+			desloc.setCoordinates( movex, movey );
+			spr.move( desloc );
+			spr.nextFrame();
+			//spr1.move( desloc );
 
 		}
 
-		//cTime = fpsTimer.getTicks();
+		// Desenhamos cada uma das camadas
+		al_hold_bitmap_drawing( true );
+
+		for( unsigned int i = 0; i < layerManager.size(); i++ ) {
+			layerManager.at( i )->draw();
+		}
+
+		texto.drawText();
+
+		al_hold_bitmap_drawing( false );
+
+		// Atualizamos a tela
+		video.refresh();
 
 		//-------------------------------------------------------
 
-		/*if( fpsTimer.getTicks() - cTime < div ) {
+		if( fpsTimer.getTicks() - cTime < div ) {
 			TimeHandler::sleep( div - fpsTimer.getTicks() );
-		}*/
+		}
 
 
-	}//while*/
-	
-	al_destroy_event_queue(event_queue);
-	al_destroy_timer(timer);
+	}//while
 
 	return 0;
 
